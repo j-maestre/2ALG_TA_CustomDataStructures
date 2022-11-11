7a2dbef660c9dbd645e535b13b9cfc4d3ed709cb
Hector Ochando <ochandoca@esat-alumni.com>
2022-11-11 17:42:53 +0100

    Make VECTOR_create

--- src/adt_vector.c
+++ src/adt_vector.c
@@ -6,4 +6,24 @@
 
 #include "EDK_MemoryManager/edk_memory_manager.h"
 
-static struct vector_ops_s vector_ops = {0};
+struct vector_ops_s vector_ops = {0};
+
+Vector* VECTOR_create(u16 capacity) {
+	if (0 >= capacity) {
+		return NULL;
+	}
+	Vector* rslt = (Vector *)MM->malloc(sizeof(Vector));
+	if (NULL == rslt) {
+		return NULL;
+	}
+	rslt->storage_ = (MemoryNode *) MM->malloc(sizeof(MemoryNode) * capacity);
+	if (NULL == rslt->storage_) {
+		MM->free(rslt);
+		return NULL;
+	}
+	rslt->head_ = 0;
+	rslt->tail_ = 0;
+	rslt->capacity_ = capacity;
+	rslt->ops_ = &vector_ops;
+	return rslt;
+}

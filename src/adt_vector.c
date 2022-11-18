1ae3604b21fdf14e2baa34c16815411405fe36ec
j-maestre <xemaiestacio@gmail.com>
2022-11-18 11:44:13 +0100

    Add new vector functions
    Add vector capacity, lenght, isEmpty, isFull, first, last and at

--- src/adt_vector.c
+++ src/adt_vector.c
@@ -139,4 +139,111 @@ s16 VECTOR_resize(Vector *vector, u16 new_size){
   
 }
 
+u16 VECTOR_capacity(Vector *vector){
+  if( NULL == vector){
+    return kErrorCode_VectorNULL;
+  }
+
+  return vector->capacity_;
+}
+
+u16 VECTOR_lenght(Vector *vector){
+  if( NULL == vector){
+    return kErrorCode_VectorNULL;
+  }
+  if(vector->tail_ > vector->capacity_){
+    return kErrorCode_VectorTailExceedsCapacity;
+  }
+  
+  return vector->tail_;
+
+}
+
+bool VECTOR_isEmpty(Vector *vector){
+  if( NULL == vector){
+    return kErrorCode_VectorNULL;
+  }
+
+  if(vector->tail_ == 0){
+    return true;
+  }
+
+  return false;
+}
+
+bool VECTOR_isFull(Vector *vector){
+  if( NULL == vector){
+    return kErrorCode_VectorNULL;
+  }
+
+  // En el caso de que cuando se añada el ultimo elemento, el tail sea +1 respecto a la capacidad
+  if(vector->tail_-1 == vector->capacity_){
+    return true;
+  }
+
+  return false;
+}
+
+
+void* VECTOR_first(Vector *vector){
+
+  if(NULL == vector){
+    return kErrorCode_VectorNULL;
+  }
+  if( NULL == vector->storage_){
+    return kErrorCode_StorageNULL;
+  }
+  if( NULL != vector->storage_->data_){
+    return vector->storage_->data_
+  }
+  return kErrorCode_DataNULL;
+
+} 
+void* VECTOR_last(Vector *vector){
+
+  if(NULL == vector){
+    return kErrorCode_VectorNULL;
+  }
+  if( NULL == vector->storage_){
+    return kErrorCode_StorageNULL;
+  }
+
+  int offset = 0;
+  if(vector->tail_ > 0){
+
+    offset = vector->tail_ -1;
+  }
+
+
+  if( NULL == (vector->storage_ + offset)->data_){
+      
+      return kErrorCode_DataNULL;
+    }
+
+  return (vector->storage_ + offset)->data_;
+} 
+void* VECTOR_at(Vector *vector, u16 position){
+
+  if(NULL == vector){
+    return kErrorCode_VectorNULL;
+  }
+  if( NULL == vector->storage_){
+    return kErrorCode_StorageNULL;
+  }
+  if(position > vector->capacity_){
+    return kErrorCode_VectorPositionExceedsCapacity;
+  }
+  if(position > vector->tail_){
+    return kErrorCode_VectorPositionExceedsTail;
+  }
+
+
+  if( NULL == (vector->storage_ + position)->data_){
+      
+      return kErrorCode_DataNULL;
+    }
+
+  return (vector->storage_ + position)->data_;
+} 
+
 

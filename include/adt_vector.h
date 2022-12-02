9d8c2d8ceaa003539f964047b92a586b974e1fd7
xema <xemaiestacio@gmail.com>
2022-12-02 18:34:37 +0100

    adding doxygen documentation

--- include/adt_vector.h
+++ include/adt_vector.h
@@ -37,32 +37,120 @@ typedef struct adt_vector_s {
 
 
 struct vector_ops_s {
-	s16 (*destroy)(Vector *vector);	// Destroys the vector and its data
-  s16 (*softReset)(Vector *vector);		// Soft resets vector's data
-  s16 (*reset)(Vector *vector);		// Resets vector's data
-	s16 (*resize)(Vector *vector, u16 new_size); // Resizes the capacity of the vector. Some elements can be lost
+  /**
+   * @brief Destroys the Vector and his data
+   * @param vector pointer to be destroyed
+   * @return s16 error code (see also ErrorCode)
+   */
+	s16 (*destroy)(Vector *vector);
+  /**
+   * @brief Soft reset per node in his storage
+   * @param vector pointer
+   * @return s16 error code (see also ErrorCode)
+   */
+  s16 (*softReset)(Vector *vector);
+
+  /**
+   * @brief Reset per node in storage
+   * @param vector pointer
+   * @return s16 error code (see also ErrorCode)
+   */
+  s16 (*reset)(Vector *vector);
+
+  /**
+   * @brief Resize the capacity of the Vector. Some elements can be lost
+   * @param vector pointer
+   * @param u16 new size capacity
+   * @return s16 error code (see also ErrorCode)
+   */
+	s16 (*resize)(Vector *vector, u16 new_size);
 
 	// State queries
-	u16 (*capacity)(Vector *vector);		// returns the maximum number of elemets to store
-	u16 (*length)(Vector *vector);			// current number of elements (<= capacity)
+
+  /**
+   * @brief Return the maxium number of elements to store
+   * @param vector pointer
+   * @return s16 size of the Vector or s16 error code (see also ErrorCode)
+   */
+	u16 (*capacity)(Vector *vector);
+
+  /**
+   * @brief Current number of elements in Storage
+   * @param Vector pointer
+   * @return u16 lenght of storage
+   */
+	u16 (*length)(Vector *vector);
+
+  /**
+   * @brief return if the vector is empty
+   * @param vector pointer
+   * @return true if is empty, false if contains any element
+   */
 	boolean (*isEmpty)(Vector *vector);
+
+  /**
+   * @brief return if the vector is full
+   * @param vector pointer
+   * @return true if the tail is equal to capacity, false if not
+   */
 	boolean (*isFull)(Vector *vector);
 
-	// Data queries
-  //? Estos devuelven datas, no el nodo
-	void* (*first)(Vector *vector); // Returns a reference to the first element of the vector
-	void* (*last)(Vector *vector); // Returns a reference to the last element of the vector-> tail -1
-	void* (*at)(Vector *vector, u16 position); // Returns a reference to the element oat a given position
+  /**
+   * @brief Returns a reference to the first element of the Vector
+   * @param vector pointer
+   * @return void *
+   */
+	void* (*first)(Vector *vector);
 
+  /**
+   * @brief Returns a reference to the last element of the Vector
+   * @param vector pointer
+   * @return void *
+   */
+	void* (*last)(Vector *vector);
 
-	// Insertion
-	s16 (*insertFirst)(Vector *vector, void *data, u16 bytes); // Inserts an element in the first position of the vector -> desplazamos todo a la derecha, cuidao si el vector está lleno, cuidao si bytes es 0
-	s16 (*insertLast)(Vector *vector, void *data, u16 bytes); // Inserts an element in the last position of the vector ->
-	s16 (*insertAt)(Vector *vector, void *data, u16 bytes, u16 position); // Inserts an element at the given position of the vector
+   /**
+   * @brief Returns a reference to the element at a given position
+   * @param vector pointer
+   * @param u16 position to get the reference
+   * @return void *
+   */
+	void* (*at)(Vector *vector, u16 position);
 
-	// Extraction
-  //? EXTRAEMOS el data_
-	void* (*extractFirst)(Vector *vector); // Extracts the first element of the vector
+  /**
+   * @brief Inserts an element in the first position of the Vector
+   * @param vector pointer
+   * @param void* data to insert at first position
+   * @param u16 bytes size of data to insert
+   * @return s16 error code (see also ErrorCode)
+   */
+	s16 (*insertFirst)(Vector *vector, void *data, u16 bytes);
+
+  /**
+   * @brief Inserts an element in the last position of the Vector
+   * @param vector pointer
+   * @param void* data to insert at last position
+   * @param u16 bytes size of data to insert
+   * @return s16 error code (see also ErrorCode)
+   */
+	s16 (*insertLast)(Vector *vector, void *data, u16 bytes);
+
+   /**
+   * @brief Inserts an element at the given position of the Vector
+   * @param vector pointer
+   * @param void* data to insert at gived position
+   * @param u16 bytes size of data to insert
+   * @param u16 position to insert the data
+   * @return s16 error code (see also ErrorCode)
+   */
+	s16 (*insertAt)(Vector *vector, void *data, u16 bytes, u16 position);
+
+  /**
+   * @brief Extracts the first element of the vector
+   * @param vector pointer
+   * @return void *
+   */
+	void* (*extractFirst)(Vector *vector);
   /*
 	// For debugging:
 #if DEBUG
@@ -74,14 +162,49 @@ struct vector_ops_s {
 #endif // DEBUG
 		);
   */
-	void* (*extractLast)(Vector *vector); // Extracts the last element of the vector
-	void* (*extractAt)(Vector *vector, u16 position); // Extracts the element of the vector at the given position
 
-	// Miscellaneous
-	s16 (*concat)(Vector *vector, Vector *vector_src); // Concatenates two vectors
-	s16 (*traverse)(Vector *vector, void (*callback)(MemoryNode *)); // Calls to a function from all elements of the vector
-	void (*print)(Vector *vector); // Prints the features and content of the vector
+ /**
+  * @brief Extracts the last element of the vector
+  * @param vector pointer
+  * @return void *
+  */
+	void* (*extractLast)(Vector *vector);
+
+  /**
+   * @brief Extracts the element of the Vector at the given position
+   * @param vector pointer
+   * @param u16 position to extract the element of the vector
+   * @return void *
+   */
+	void* (*extractAt)(Vector *vector, u16 position);
+
+	/**
+	 * @brief Concatenates two vectors
+	 * @param vector pointer base
+	 * @param vector pointer to concatenate
+   * @return s16 error code (see also ErrorCode)
+	 */
+	s16 (*concat)(Vector *vector, Vector *vector_src);
+
+  /**
+	 * @brief Calls to a function from all elements of the Vector
+	 * @param vector pointer
+   * @param callback pointer to call per each data in Storage
+   * @return s16 error code (see also ErrorCode)
+	 */
+	s16 (*traverse)(Vector *vector, void (*callback)(MemoryNode *));
+
+  /**
+   * @brief Prints the features and content of the vector
+   * @param vector pointer
+   */
+	void (*print)(Vector *vector);
 };
 
-Vector* VECTOR_create(u16 capacity); // Creates a new vector
+/**
+ * @brief Creates a new vector
+ * @param u16 capacity of the Vector 
+ * @return Vector * 
+ */
+Vector* VECTOR_create(u16 capacity);
 #endif //__ADT_VECTOR_H__

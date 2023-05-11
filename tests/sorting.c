67773104ef0bb3db5c5a67757f8efd972b7ad26f
j-maestre <xemaiestacio@gmail.com>
2023-05-11 12:16:43 +0200

    finish quicksort algoritm

--- tests/sorting.c
+++ tests/sorting.c
@@ -10,101 +10,91 @@
 #include "adt_dllist.h"
 
 Vector *v = NULL;
-
-void swap(u16 first, u16 second){
-  
-
-}
-
- 
-int partition(Vector *v, u16 start, u16 end)
-{
- 
-    u16 pivot = v->ops_->at(v,start);
- 
-    u16 count = 0;
-    for (u16 i = start + 1; i <= end; i++) {
-        if (v->ops_->at(v,i) <= pivot)
-            count++;
-    }
- 
-    // Giving pivot element its correct position
-    u16 pivotIndex = start + count;
-    swap(v->ops_->at(v,pivotIndex), v->ops_->at(v, start));
- 
-    // Sorting left and right parts of the pivot element
-    u16 i = start, j = end;
- 
-    while (i < pivotIndex && j > pivotIndex) {
- 
-        while (v->ops_->at(v,i) <= pivot) {
-            i++;
-        }
- 
-        while (v->ops_->at(v,j) > pivot) {
-            j--;
-        }
- 
-        if (i < pivotIndex && j > pivotIndex) {
-          swap(i++, j--);
-        }
-    }
- 
-    return pivotIndex;
-}
- 
-void quickSort(Vector *v, u16 start, u16 end)
-{
- 
-    // base case
-    if (start >= end)
-        return;
- 
-    // partitioning the array
-    u16 p = partition(v, start, end);
- 
-    // Sorting the left part
-    quickSort(v, start, p - 1);
- 
-    // Sorting the right part
-    quickSort(v, p + 1, end);
-}
- 
 u16 capacity;
 u16 data[5000];
-u16 data2[5000];
 u16 max_range;
-void InitDataForTest(){
-  srand(time(NULL));
-  capacity = 5000;
-  max_range = 150;
-  v = NULL;
-  v = VECTOR_create(capacity);
 
-  for(u16 i = 0; i < capacity; i++){
-		u16 *number = MM->malloc(sizeof(u16));
-		u16 *number2 = MM->malloc(sizeof(u16));
-    int r = rand()%max_range;
-  	*number = r;
-  	*number2 = r;
-		data[i] = number;
-		data2[i] = number2;
-    v->ops_->insertFirst(v,number,sizeof(u16));
 
-	}
+void swap(Vector *vec, u16 firstPos, u16 secondPos){
+  u16 *firstValue = (u16*) vec->ops_->at(vec,firstPos); 
+  u16 *secondValue = (u16*) vec->ops_->at(vec,secondPos);
+
+  u16 aux = *firstValue;
+  *firstValue = *secondValue;
+  *secondValue = aux; 
+
 }
 
+ 
+// function to find the partition position
+u16 partition(Vector *v, int low, int high) {
+  
+  u16 *pivot = (u16*) v->ops_->at(v,high); 
+
+  u16 i = (low - 1);
+
+  for (u16 j = low; j < high; j++) {
+    //if (array[j] <= *pivot) {
+    if (*((u16*) v->ops_->at(v,j)) <= *pivot) {
+      //i++;
+      swap(v,++i,j);
+    }
+  }
+
+  swap(v,i+1,high);
+  
+  // return the partition point
+  return (i + 1);
+}
+ 
+void quickSort(Vector *v, int low, int high) {
+  if (low < high) {
+    
+    int pi = partition(v, low, high);
+    
+    // recursive call on the left of pivot
+    quickSort(v, low, pi - 1);
+    
+    // recursive call on the right of pivot
+    quickSort(v, pi + 1, high);
+  }
+}
+ 
+
+void InitDataForTest(){
+  
+    capacity = 100;
+    max_range = 150;
+    v = NULL;
+    v = VECTOR_create(capacity);
+
+    for(u16 i = 0; i < capacity; i++){
+        u16 *number = MM->malloc(sizeof(u16));
+        int r = rand()%max_range;
+        *number = r;
+        v->ops_->insertFirst(v,number,sizeof(u16));
+    }
+}
+
+
 int main()
-{
+{   
+    srand(time(NULL));
     InitDataForTest();
-    int arr[] = { 9, 3, 4, 2, 1, 8 };
-    u16 n = 6;
  
-    quickSort(v, 0, max_range - 1);
+    printf("*** Un-ordered ***\n");
+    for (u16 i = 0; i < capacity; i++) {
+        u16 *num = (u16*) v->ops_->at(v,i);
+        printf("%d - ",*num);
+
+    }
+    printf("\nSorting...\n");
+    quickSort(v, 0, capacity - 1);
  
-    for (u16 i = 0; i < max_range; i++) {
-      printf("%d - ",v->ops_->at(v,i));
-       // cout << arr[i] << " ";
+    printf("*** Ordered ***\n");
+    for (u16 i = 0; i < capacity; i++) {
+        u16 *num = (u16*) v->ops_->at(v,i);
+        printf("%d - ",*num);
     }
  
     return 0;
